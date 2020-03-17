992ee3c08f208ba75f0466f21e3c526d88f7269e
Artur Mustafin <artur.mustafin@gmail.com>
2020-03-18 00:15:10 +0300

    added some fixes

--- main.c
+++ main.c
@@ -6,14 +6,35 @@ typedef void* payload_ptr;
 
 typedef struct q_type {
     q_type_ptr next;
-    q_type_ptr prev;
+    q_type_ptr tail;
     payload_ptr payload;
 } q_type;
 
 q_type_ptr q_push(q_type_ptr queue, q_type_ptr q_ptr) {
-    q_ptr->prev = queue->next;
-    queue->next = q_ptr;
+    if (q_ptr != 0){
+        q_ptr->next = queue->next;
+        if (queue->next != 0) queue->next->tail = q_ptr;
+        queue->next = q_ptr;
+        if (queue->tail == 0) queue->tail = q_ptr;
+    }
     return q_ptr;
+}
+
+q_type_ptr q_pop(q_type_ptr q_ptr) {
+    if (q_ptr != 0) {
+        q_type_ptr queue = q_ptr->tail;
+        if (queue != 0 ) {
+            q_ptr->tail = queue->tail;
+            if (queue->tail != 0) queue->tail->next = 0;
+            queue->tail = queue->next = 0;
+            if (queue == q_ptr->next) q_ptr->next = 0;
+        } else {
+            q_ptr->tail = q_ptr->next = 0;
+            return q_ptr;
+        }
+        return queue;
+    }
+    return 0;
 }
 
 q_type_ptr create_node(payload_ptr payload) {
@@ -23,36 +44,31 @@ q_type_ptr create_node(payload_ptr payload) {
 }
 
 void print_tree(q_type_ptr q_ptr) {
-    if (q_ptr != 0) {
-        print_tree(q_ptr->prev);
+    while (q_ptr != 0) {
         if (q_ptr->payload != 0) {
-            printf("0x%llx\n", q_ptr->payload);
+            printf("0x%llx 0x%llx\n", q_ptr, q_ptr->payload);
         }
-        print_tree(q_ptr->next);
-    }
+        q_ptr = q_ptr->tail;
+    } 
+    printf("\n");
 }
 
 void q_free(q_type_ptr q_ptr) {
-    if (q_ptr != 0) {
-        q_free(q_ptr->prev);       
-        q_free_ptr(&(q_ptr->prev));
-        q_free(q_ptr->next);
-        q_free_ptr(&(q_ptr->next));
+    while (q_ptr != 0) {
+        q_type_ptr next = q_ptr->tail;
+        free(q_ptr);
+        printf("0x%llx freed\n", q_ptr);
+        q_ptr = next;
     }
-}
-
-void q_free_ptr(q_type_ptr *q_ptr_addr) {
-    if (*q_ptr_addr != 0) {
-        free(*q_ptr_addr);
-        printf("0x%llx freed\n", *q_ptr_addr);
-        *q_ptr_addr = 0;
-    }
+    printf("\n");
 }
 
 int main() {
     payload_ptr payload = (payload_ptr)0xdeadbeef;
-    q_type_ptr q_ptr0, q_ptr1, q_ptr2, q_ptr3, q_ptr4, q_ptr5, q_ptr6;
-    
+
+    q_type_ptr q_ptr0, q_ptr1, q_ptr2, q_ptr3, q_ptr4; //, q_ptr5, q_ptr6;
+    q_type_ptr q_pop0, q_pop1, q_pop2, q_pop3, q_pop4;
+
     q_ptr0 = create_node(0);
 
     q_ptr1 = q_push(q_ptr0, create_node(payload++));
@@ -60,11 +76,17 @@ int main() {
     q_ptr3 = q_push(q_ptr0, create_node(payload++));
     q_ptr4 = q_push(q_ptr0, create_node(payload++));
 
-    q_ptr5 = q_push(q_ptr2, create_node(payload++)); //technically, insert 2 more points from the point q_ptr2, into queue  
-    q_ptr6 = q_push(q_ptr2, create_node(payload++)); 
+    // q_ptr5 = q_push(q_ptr2, create_node(payload++)); //technically, insert 2 more points at the point q_ptr2, into queue  
+    // q_ptr6 = q_push(q_ptr2, create_node(payload++)); 
 
     print_tree(q_ptr0);
 
+    q_pop0 = q_pop(q_ptr0); 
+    // q_pop1 = q_pop(q_ptr0);
+    // q_pop2 = q_pop(q_ptr0);
+    // q_pop3 = q_pop(q_ptr0);
+    // q_pop4 = q_pop(q_ptr0);
+
     q_free(q_ptr0);
-    q_free_ptr(&q_ptr0);
+    q_free(q_pop0);
 }
