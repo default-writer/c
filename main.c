377fcf92249150a6760c3b8ea4b359c2309f4480
Artur Mustafin <artur.mustafin@gmail.com>
2020-08-04 07:12:43 +0000

    comment out values

--- main.c
+++ main.c
@@ -83,7 +83,7 @@ void q_type_free(q_type_context * const ctx, q_type_ptr * const item) {
     q_type_ptr* head = &(ctx->head);
     // gets pre-allocated (compiler-generated) stack value as temporary
     q_type_ptr tmp;
-    // otherwize, assign currently selected item pointer to temporary
+    // assign currently selected item pointer to temporary
     tmp.ptr = item->ptr;
     // until we run out of stack or stop at root element
     while (ctx->count > 0 && tmp.ptr != 0) {
@@ -93,11 +93,11 @@ void q_type_free(q_type_context * const ctx, q_type_ptr * const item) {
 #endif
         // gets temporary pointer value
         q_type* ptr = tmp.ptr;
-        // advances tamporary poinnter value to the next to current item
+        // advances temporary pointer value to the next item
         tmp.ptr = tmp.ptr->next.ptr;
         // free temporary pointer value
         free(ptr);
-        // updates current context counter
+        // decrement current context counter
         ctx->count--;
     }
     // all stack items are processed
@@ -107,37 +107,66 @@ void q_type_free(q_type_context * const ctx, q_type_ptr * const item) {
 #endif
 }
 
+// allocates a memory for provided payload 
+// at current context, data payload stored at allocated memory buffer
+// as a result, items counter will increase
 void q_type_alloc(q_type_context * const ctx, abstract_ptr payload) {
+    // get current context's head
     q_type_ptr* head = &(ctx->head);
+    // gets pre-allocated (compiler-generated) stack value as temporary
     q_type_ptr tmp;
+    // stores into pre-allocated value newly allocated memory buffer pointer
     tmp.ptr = (q_type*)malloc(sizeof(q_type));
+    // sets the new data into allocated memory buffer
     tmp.ptr->payload = payload;
 #ifdef DEBUG
+    // visualise loop break
     printf("alloc: 0x%llx 0x%llx\n", (ADDR)tmp.ptr, (ADDR)tmp.ptr->payload);
 #endif
+    // pushes new item on top of the stack in current context
     q_type_push(ctx, &tmp);
+    // increment current context's counter by one
     ctx->count++;
 }
 
+// initializes the new context's root element
+// as a result, new memory block will be allocated
+// current context pointer set to zero
 void q_type_init(q_type_context * const ctx) {
+    // stores into pre-allocated value newly allocated memory buffer pointer
     ctx->head.ptr = (q_type*)malloc(sizeof(q_type));
+    // sets current context's counter to zero
     ctx->count = 0;
 }
 
+// destroys the memory stack
+// frees all memory elements
+// as a result, memory will be freed
 void q_type_destroy(q_type_context * const ctx) {
+    // get current context's head
     q_type_ptr* head = &(ctx->head);
+    // gets pre-allocated (compiler-generated) stack value as temporary
     q_type_ptr tmp;
+    // assign currently selected item pointer to temporary
     tmp.ptr = head->ptr;
+    // until we run out of stack or stop at root element
     while (ctx->count > 0 && tmp.ptr != 0) {
 #ifdef DEBUG
+        // debug output of memory dump
         printf("free: 0x%llx 0x%llx\n", (ADDR)tmp.ptr, (ADDR)tmp.ptr->payload);
 #endif
+        // gets temporary pointer value
         q_type* ptr = tmp.ptr;
+        // advances temporary pointer value to the previous value
         tmp.ptr = tmp.ptr->prev.ptr;
+        // free temporary pointer value
         free(ptr);
+        // decrements current context's counter
         ctx->count--;
     }
+    // all stack items are processed
 #ifdef DEBUG
+    // visualise loop break 
     printf("\n");
 #endif
 }
