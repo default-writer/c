f915883513aebfc6ad347671e0c045e3f3d63388
default-writer <funcelot@devweb.app>
2022-10-30 13:48:02 +0300

    fix tools install

--- install_tools.ps1
+++ install_tools.ps1
@@ -1,1 +1,2 @@
-& choco install ninja lcov --force -y --execution-timeout 3600 
+& choco install ninja --force -y --execution-timeout 3600 
+& choco install lcov --force -y --execution-timeout 3600 
