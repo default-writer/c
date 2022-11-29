61865cda4011af9ee6fc08f546cfca3e64546390
default-writer <funcelot@devweb.app>
2022-11-29 11:14:50 +0300

    added coverage info

---
+++ bin/coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+pwd=$(pwd)
+
+gcc --coverage -g \
+    ${pwd}/tests/main.c \
+    ${pwd}/src/list/code.c \
+    ${pwd}/src/common/alloc.c \
+    ${pwd}/src/common/print.c \
+    ${pwd}/src/common/object.c \
+    -I${pwd}/src/ \
+    -I${pwd}/src/std/ \
+    -I${pwd}/src/common/ \
+    -I${pwd}/src/list/ \
+    -I${pwd}/rexo/include/ \
+    -o ${pwd}/build/main \
+    && lcov --capture --directory ${pwd}/ --output-file main.info
+
+cd ${pwd}
