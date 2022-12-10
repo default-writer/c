3e7ca5a575d2cb1ab1b8bc3f1122fc8e136a1f7a
default-writer <funcelot@outlook.com>
2022-12-10 10:36:28 +0000

    code refactoring

--- src/common/object.c
+++ src/common/object.c
@@ -18,7 +18,7 @@ void list_init(struct list_data** const current, struct list_data* (*_new)()) {
 /* destroys the memory stack */
 /* frees all memory elements */
 /* as a result, memory will be freed */
-void list_destroy(struct list_data** const current, void (*delete)(struct list_data*), struct list_data* (*_next)(struct list_data*)) {
+void list_destroy(struct list_data** const current, void (*_delete)(struct list_data*), struct list_data* (*_next)(struct list_data*)) {
     /* get current context's head */
     /* assigns currently selected item pointer to temporary */
     struct list_data* tmp = *current;
@@ -31,7 +31,7 @@ void list_destroy(struct list_data** const current, void (*delete)(struct list_d
             /* gets prev pointer value */
             struct list_data* next = _next(ptr);
             /* frees up memory, should check for 0 before execution */
-            delete(ptr);
+            _delete(ptr);
             /* advances temporary pointer value to the next item */
             tmp = next;
         } while (tmp != 0);
