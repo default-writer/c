fe1884b8054994ff4a730eb7d3f4686661ca4a40
default-writer <funcelot@outlook.com>
2022-12-10 10:34:56 +0000

    refactoring

--- src/common/print.c
+++ src/common/print.c
@@ -8,17 +8,17 @@
 struct list_data;
 
 // print head on current context (stack)
-void list_print_head(struct list_data** const current, void* (*data)(struct list_data*)) {
+void list_print_head(struct list_data** const current, void* (*_data)(struct list_data*)) {
     // get current context's head
     struct list_data* tmp = *current;
     // visualise item
-    printf("*: 0x%llx 0x%llx\n", (ADDR)tmp, (ADDR)data(tmp));
+    printf("*: 0x%llx 0x%llx\n", (ADDR)tmp, (ADDR)_data(tmp));
 }
 
 // print all stack trace to output
 // in a single loop, print out all elements except root element (which does not have a payload)
 // as a result, all stack will be printed in last-to-first order (reverse)
-void list_print(struct list_data** const current, struct list_data* (*next)(struct list_data*)) {
+void list_print(struct list_data** const current, struct list_data* (*_next)(struct list_data*)) {
     // get current context's head
     struct list_data* head = *current;
     // sets the counter
@@ -32,7 +32,7 @@ void list_print(struct list_data** const current, struct list_data* (*next)(stru
             // debug output of memory dump
             printf("%d: 0x%llx 0x%llx\n", ++i, (ADDR)tmp, (ADDR)_data(tmp));
             // remember temprary's prior pointer value to temporary
-            tmp = next(tmp);
+            tmp = _next(tmp);
         } while (tmp != 0/*root*/);
     }
     // stop on root element
