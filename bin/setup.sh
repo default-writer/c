b3390c29616750734b55d02d5c3653a4c7c21e88
default-writer <funcelot@outlook.com>
2022-12-11 08:54:28 +0300

    added bazel support to extensions

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,13 +11,19 @@ pwd=$(pwd)
 
 install="$1"
 
-if [ "${install}" == "build" ]
+if [ "${install}" == "python" ]
 then
   git config --global --add safe.directory ${pwd}
   git config --global pull.rebase false
 
   apt-get update
   apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
+fi
+
+if [ "${install}" == "cmake" ]
+then
+  apt-get update
+  apt-get -y install --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov 
 fi
 
 if [ "${install}" == "docker" ]
