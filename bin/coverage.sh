6dc583c680be1fdce8669cc0d97ecb8bdc0f9eaa
default-writer <funcelot@outlook.com>
2022-12-13 21:13:41 +0300

    added ode coverage on start

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -11,27 +11,27 @@ rm -rf "${pwd}/coverage"
 mkdir "${pwd}/coverage"
 
 ## define an array ##
-array=('' 'light' 'micro')
+array=('' '-light' '-micro')
 
 ## get item count using ${array[@]} ##
 for m in ${array[@]}; do
 	gcc --coverage -g \
-		"${pwd}/tests/main-${m}.c" \
-		"${pwd}/src/list-${m}/code.c" \
+		"${pwd}/tests/main${m}.c" \
+		"${pwd}/src/list${m}/code.c" \
 		"${pwd}/src/common/alloc.c" \
 		"${pwd}/src/common/print.c" \
 		"${pwd}/src/common/object.c" \
 		-I"${pwd}/src/" \
 		-I"${pwd}/src/std/" \
 		-I"${pwd}/src/common/" \
-		-I"${pwd}/src/list-${m}/" \
+		-I"${pwd}/src/list${m}/" \
 		-I"${pwd}/rexo/include/" \
 		-DMEMORY_DEBUG_INFO \
 		-DMEMORY_CLEANUP \
 		-DMEMORY_LEAKS \
-		-o "${pwd}/coverage/main-${m}" &&
-		"${pwd}/coverage/main-${m}" &&
-		lcov --capture --directory "${pwd}/coverage/" --output-file "${pwd}/coverage/main-${m}.info"
+		-o "${pwd}/coverage/main${m}" &&
+		"${pwd}/coverage/main${m}" &&
+		lcov --capture --directory "${pwd}/coverage/" --output-file "${pwd}/coverage/main${m}.info"
 done
 
 cd "${pwd}"
