0e5c622b8c1fa789cbaccd5df4ceb17203459555
default-writer <funcelot@outlook.com>
2022-12-15 19:51:10 +0300

    added pyenv requirements install

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,21 +12,29 @@ pwd=$(pwd)
 
 install="$1"
 
+if [ "${install}" == "pyenv" ]; then
+	git config --global --add safe.directory "${pwd}"
+	git config --global pull.rebase false
+
+	apt-get update -y
+	apt-get install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
+fi
+
 if [ "${install}" == "python" ]; then
 	git config --global --add safe.directory "${pwd}"
 	git config --global pull.rebase false
 
-	apt-get update
+	apt-get update -y
 	apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
 fi
 
 if [ "${install}" == "cmake" ]; then
-	apt-get update
+	apt-get update -y
 	apt-get -y install --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov
 fi
 
 if [ "${install}" == "docker" ]; then
-	apt-get update
+	apt-get update -y
 	apt-get install -y \
 		ca-certificates \
 		curl \
