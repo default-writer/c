bde6530872fc87fb2329c4e67cd835290912044d
default-writer <funcelot@outlook.com>
2022-12-16 15:08:30 +0300

    removed activation step

--- bin/venv.sh
+++ bin/venv.sh
@@ -15,15 +15,11 @@ install="$1"
 if [ "${install}" == "venv" ]; then
 	rm -rf venv
 	python3 -m venv venv
-	. venv/bin/activate
-	python3 -m pip install --upgrade pip
-	[ -f "requirements.txt" ] && python3 -m pip install -r requirements.txt
 fi
 
 if [ "${install}" == "pyenv" ]; then
 	pyenv install -f 3.11.1
 	pyenv virtualenv -f 3.11.1 .venv
-	pyenv activate .venv
 fi
 
 cd "${pwd}"
