4623fafcad4f645526587f1479bce2d53e29a965
default-writer <funcelot@outlook.com>
2022-12-16 14:36:19 +0300

    added support to pyenv activate

--- bin/venv.sh
+++ bin/venv.sh
@@ -10,20 +10,22 @@ fi
 
 pwd=$(pwd)
 
-"${pwd}/bin/clean.sh"
+install="$1"
 
-rm -rf venv
-
-if [ ! -d "venv" ]; then
+if [ "${install}" == "venv" ]; then
+	rm -rf venv
 	python3 -m venv venv
-
 	. venv/bin/activate
-
 	python3 -m pip install --upgrade pip
-
 	[ -f "requirements.txt" ] && python3 -m pip install -r requirements.txt
-	[ -f "dev_requirements.txt" ] && python3 -m pip install -r dev_requirements.txt
-
 fi
+
+if [ "${install}" == "pyenv" ]; then
+	pyenv insall 3.11.1
+	pyenv virtualenv 3.11.1 .venv
+	pyenv activate .venv
+fi
+
+
 
 cd "${pwd}"
