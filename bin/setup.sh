056a6d649531100636184c43089f8081ddd97e7b
default-writer <funcelot@outlook.com>
2022-12-16 05:32:27 +0300

    added comments section

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,30 +12,37 @@ pwd=$(pwd)
 
 install="$1"
 
+# you can use command `apt install --only-upgrade`` for packages wouldn't upgrade since they are marked as manual installed packages
+
 if [ "${install}" == "pyenv" ]; then
 	git config --global --add safe.directory "${pwd}"
 	git config --global pull.rebase false
 
-	apt-get update -y
-	apt-get install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev
+	apt update -y
+	apt install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev
+	apt install -y --only-upgrade apport apport-gtk python3-apport python3-problem-report
+	apt upgrade -y
 fi
 
 if [ "${install}" == "python" ]; then
 	git config --global --add safe.directory "${pwd}"
 	git config --global pull.rebase false
 
-	apt-get update -y
+	apt update -y
 	apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave python3-virtualenv
+	apt install -y --only-upgrade apport apport-gtk python3-apport python3-problem-report
+	apt upgrade -y
 fi
 
 if [ "${install}" == "cmake" ]; then
-	apt-get update -y
-	apt-get -y install --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov
+	apt update -y
+	apt install -y --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov
+	apt upgrade -y
 fi
 
 if [ "${install}" == "docker" ]; then
-	apt-get update -y
-	apt-get install -y \
+	apt update -y
+	apt install -y \
 		ca-certificates \
 		curl \
 		gnupg \
@@ -46,11 +53,12 @@ if [ "${install}" == "docker" ]; then
 		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-	apt-get update -y
+	apt update -y
 	chmod a+r /etc/apt/keyrings/docker.gpg
 
-	apt-get update -y
-	apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	apt update -y
+	apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	apt upgrade -y
 fi
 
 cd "${pwd}"
