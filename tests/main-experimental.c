e8b6146c42d122250011d4e59deb07ee40efe524
default-writer <funcelot@outlook.com>
2022-12-20 10:51:18 +0300

    code cleanup

--- tests/main-experimental.c
+++ tests/main-experimental.c
@@ -12,7 +12,6 @@ const char* __asan_default_options() { return "detect_leaks=0"; }
 extern const struct list_methods list_methods_experimental;
 
 struct list_data* new_list() {
-    const struct list_methods* list = &list_methods_experimental;
     // init list
     struct list_data* ptr = _new();
     // setup stack
@@ -22,7 +21,6 @@ struct list_data* new_list() {
 }
 
 void delete_list(struct list_data** ctx) {
-    const struct list_methods* list = &list_methods_experimental;
     // gets pointer
     struct list_data* ptr = *ctx;
     // destroys list
@@ -96,7 +94,7 @@ void using_list2(void (*list_using)(struct list_data** const)) {
 void list_using(struct list_data** const current) {
     // access context's functions pointer
     const struct list_methods* list = &list_methods_experimental;
-    LPTR* payload = (LPTR*)0xdeadbeef;
+    const LPTR* payload = (LPTR*)0xdeadbeef;
     const void* is_null[] = {
         list->peek(current),
         list->pop(current)
@@ -192,8 +190,6 @@ typedef struct test_data {
 RX_SET_UP(test_set_up) {
     TEST_DATA rx = (TEST_DATA)RX_DATA;
     struct list_data** ctx = &rx->ctx;
-    // access context's functions pointer
-    const struct list_methods* list = &list_methods_experimental;
 
     // initialize list
     *ctx = new_list();
@@ -204,8 +200,6 @@ RX_SET_UP(test_set_up) {
 RX_TEAR_DOWN(test_tear_down) {
     TEST_DATA rx = (TEST_DATA)RX_DATA;
     struct list_data** ctx = &rx->ctx;
-    // access context's functions pointer
-    const struct list_methods* list = &list_methods_experimental;
 
     /* get current context's head */
     struct list_data* ptr = *ctx;
@@ -232,7 +226,6 @@ RX_TEST_CASE(myTestSuite, test_empty_list_pop_equals_0, .fixture = test_fixture)
 
     // create list
     const struct list_methods* list = &list_methods_experimental;
-
     const struct list_data* head = list->pop(&ctx);
  
     // enshure that counter is initialized to 0
@@ -246,7 +239,6 @@ RX_TEST_CASE(myTestSuite, test_empty_list_peek_equals_0, .fixture = test_fixture
 
     // create list
     const struct list_methods* list = &list_methods_experimental;
-
     const struct list_data* head = list->peek(&ctx);
  
     // enshure that counter is initialized to 0
@@ -319,7 +311,6 @@ RX_TEST_CASE(myTestSuite, test_list_peek_is_zero, .fixture = test_fixture) {
 
     // create list
     const struct list_methods* list = &list_methods_experimental;
-
     const void* head = list->peek(ctx);
 
     // ensure that data being added to list
@@ -333,7 +324,6 @@ RX_TEST_CASE(myTestSuite, test_list_pop_is_zero, .fixture = test_fixture) {
 
     // create list
     const struct list_methods* list = &list_methods_experimental;
-
     const void* head = list->pop(ctx);
 
     // ensure that data being added to list
