be4391a65cd26484dcb0b8ad3b63b42f11819d6b
default-writer <funcelot@outlook.com>
2022-12-20 06:24:45 +0300

    added gh cli installation script

--- bin/setup.sh
+++ bin/setup.sh
@@ -14,6 +14,15 @@ install="$1"
 
 # you can use command `apt install --only-upgrade`` for packages wouldn't upgrade since they are marked as manual installed packages
 
+if [ "${install}" == "gh" ]; then
+	type -p curl >/dev/null || apt install curl -y
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& apt update \
+	&& apt install gh -y
+fi
+
 if [ "${install}" == "git" ]; then
 	apt update -y
 	apt install -y git
