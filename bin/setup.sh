d10fa58a20e3f2e3beb6ab27a0ca0777bdc63c2b
default-writer <funcelot@outlook.com>
2022-12-29 08:43:58 -0500

    added update & upgrade functions

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,65 +12,71 @@ pwd=$(pwd)
 
 install="$1"
 
-apt install -y --fix-broken
-apt update -y --fix-missing
-apt upgrade -y
-apt autoremove -y
+function update() {
+    apt install -y --fix-broken
+    apt update -y --fix-missing
+}
+
+function upgrade() {
+    apt upgrade -y
+    apt autoremove -y
+}
 
 case "${install}" in
 
     "--llvm") # installs llvm and llvm-cov
-        apt update -y
+        update
         apt install -y llvm
-        apt upgrade -y
+        upgrade
         ;;
 
     "--zsh") # installs zsh
-        apt update -y
+        update
         apt install -y zsh
-        apt upgrade -y
+        upgrade
         chsh -s $(which bash)
+        upgrade
         ;;
 
     "--gh") # installs gh
-        apt update -y
+        update
         type -p curl >/dev/null || apt install curl -y
-        curl --silent -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/shar\(e/keyrings/githubcli-archive-keyring.gpg \
-        && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-        && apt update -y \
-        && apt install gh -y
-        apt upgrade -y
+        curl --silent -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/shar\(e/keyrings/githubcli-archive-keyring.gpg
+        chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        update
+        apt install gh -y
+        upgrade
         ;;
 
     "--git") # install git
-        apt update -y
+        update
         apt install -y git
-        apt upgrade -y
+        upgrade
         ;;
 
     "--pyenv") # installs pyenv
-        apt update -y
+        update
         apt install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev
         apt install -y --only-upgrade apport apport-gtk python3-apport python3-problem-report
-        apt upgrade -y
+        upgrade
         ;;
 
     "--python") # installs python
-        apt update -y
+        update
         apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave python3-virtualenv
         apt install -y --only-upgrade apport apport-gtk python3-apport python3-problem-report
-        apt upgrade -y
+        upgrade
         ;;
 
     "--cmake") # installs cmake
-        apt update -y
+        update
         apt install -y --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov ninja-build
-        apt upgrade -y
+        upgrade
         ;;
 
     "--docker") # installs docker
-        apt update -y
+        update
         apt install -y \
             ca-certificates \
             curl \
@@ -79,20 +85,20 @@ case "${install}" in
         mkdir -p /etc/apt/keyrings
         curl --silent -fsSL --use-ascii --retry 5 --retry-all-errors https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
-        apt update -y
+        update
         chmod a+r /etc/apt/keyrings/docker.gpg
-        apt update -y
         apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-        apt upgrade -y
+        upgrade
         ;;
     
     "--docker-compose") # installs docker-compose
-        apt update -y
+        update
         apt install -y --fix-broken qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
         systemctl enable --now libvirtd
         curl -L https://desktop.docker.com/linux/main/amd64/docker-desktop-4.15.0-amd64.deb -o /tmp/docker-desktop-4.15.0-amd64.deb
+        update
         dpkg -i /tmp/docker-desktop-4.15.0-amd64.deb
-        apt upgrade -y
+        upgrade
         ;;
 
     *)
