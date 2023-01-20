8f287148ecb9cfe18627132e60694c2d30e2417f
default-writer <funcelot@outlook.com>
2023-01-20 20:24:25 +0300

    [master] code refactorings

--- tests/main-brain.c
+++ tests/main-brain.c
@@ -1,15 +1,12 @@
 #include "rexo/include/rexo.h"
 #include "std/common.h"
 #include "playground/brain/brain.h"
-#include "types.h"
-#include <linux/limits.h>
-#include <string.h>
 
 void process(char* data) {
     printf("%s\n", data);
 }
 
-void get_full_path(int argc, char** argv, char* path, char* file_name) {
+void get_full_path(int argc, char** argv, char* path, const char* file_name) {
     if (argc > 0) {
         strcpy(path, argv[0]);
     }
@@ -32,7 +29,7 @@ int main(int argc, char** argv) {
     FILE *f;
     get_full_path(argc, argv, cwd, "/input.txt");
     if ((f = fopen(cwd, "rb")) != 0) {
-        long size = get_file_size(f);
+        u32 size = (u32)get_file_size(f);
         char *data = calloc(1, size + 1);
         fread(data,1,size,f);
         fclose(f);
