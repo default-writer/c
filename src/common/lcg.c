4a0ec162aa56ee072984151942c9e0d84681a6a1
default-writer <funcelot@outlook.com>
2023-01-24 12:32:47 +0300

    [master] fix parensis

--- src/common/lcg.c
+++ src/common/lcg.c
@@ -1,10 +1,10 @@
 #include "std/common.h"
 
 /* LCG Park-Miller state */
-u64 lcg_state = 0xdeadbeef & 0x7fffffff - 1; // 0x5eadbeee
+u64 lcg_state = (0xdeadbeef & 0x7fffffff) - 1; // 0x5eadbeee
 
 /* LCG Park-Miller function */
-u64 lcg_parkmiller() {
+u32 lcg_parkmiller() {
     lcg_state = lcg_state * 48271 % 0x7fffffff;
-    return lcg_state;
+    return (u32)lcg_state;
 }
