efa2b485462af852c58cf546c50400e3898d9f76
default-writer <funcelot@outlook.com>
2023-02-01 07:15:13 +0300

    [master] code refactoring

--- src/playground/test.c
+++ src/playground/test.c
@@ -1,16 +1,16 @@
 #include "std/common.h"
 
-static char itoa(int v) { return (char)(v < 10 ? '0' + v : 'a' + (v - 10)); }
+static char itoa(u8 v) { return (char)(v < 10 ? '0' + v : v<16 ? 'a' + (v - 10) : '0'); }
 
-static void dec_to_hex(int value, char* const lo, char* const hi) {
+static void dec_to_hex(u8 value, char* const lo, char* const hi) {
     *lo = itoa(value % 16);
     *hi = itoa(value / 16);
 }
 
 static int rgb(u8 r, u8 g, u8 b, char* const output) {
-    dec_to_hex(b, &output[5], &output[4]);
-    dec_to_hex(g, &output[3], &output[2]);
-    dec_to_hex(r, &output[1], &output[0]);
+    dec_to_hex(b % 256, &output[5], &output[4]);
+    dec_to_hex(g % 256, &output[3], &output[2]);
+    dec_to_hex(r % 256, &output[1], &output[0]);
     return 0;
 }
 
