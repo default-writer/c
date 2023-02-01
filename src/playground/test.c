23255625c241fe0a6f6eca4c6e1c34d64f4d17ca
default-writer <funcelot@outlook.com>
2023-02-01 07:00:32 +0300

    [master] updated test

--- src/playground/test.c
+++ src/playground/test.c
@@ -1,106 +1,24 @@
 #include "std/common.h"
-
-void int_to_char(int input, int pointer, char* output) {
-    switch (input) {
-    case 0:
-        output[pointer] = '0';
-        break;
-    case 1:
-        output[pointer] = '1';
-        break;
-    case 2:
-        output[pointer] = '2';
-        break;
-    case 3:
-        output[pointer] = '3';
-        break;
-    case 4:
-        output[pointer] = '4';
-        break;
-    case 5:
-        output[pointer] = '5';
-        break;
-    case 6:
-        output[pointer] = '6';
-        break;
-    case 7:
-        output[pointer] = '7';
-        break;
-    case 8:
-        output[pointer] = '8';
-        break;
-    case 9:
-        output[pointer] = '9';
-        break;
-    case 10:
-        output[pointer] = 'A';
-        break;
-    case 11:
-        output[pointer] = 'B';
-        break;
-    case 12:
-        output[pointer] = 'C';
-        break;
-    case 13:
-        output[pointer] = 'D';
-        break;
-    case 14:
-        output[pointer] = 'E';
-        break;
-    case 15:
-        output[pointer] = 'F';
-        break;
-    default:
-        output[0] = 'x';
-        output[1] = 'x';
-        output[2] = 'x';
-    }
-}
-
-int aprx(int input) {
-    if (input < 0) {
-        return 0;
-    } else if (input > 255) {
-        return 255;
-    } else {
-        return input;
-    }
-}
-
-// указатель в плане смещения относительно конца массива
-int pointer = 0; 
+#include <string.h>
 
 // char* output - выходной массив
-void dec_to_hex(int dec_input, char* output) {
-    int_to_char((dec_input % 16), pointer, &output[0]);
-    dec_input = dec_input / 16;
-    pointer = pointer - 1;
-    int_to_char((dec_input % 16), pointer, &output[0]);
-    pointer = pointer - 1;
+static void dec_to_hex(int dec_input, char* const lo, char* const hi) {
+    int low = (dec_input % 16);
+    int high = dec_input / 16;
+    *lo = (char)(low < 10 ? '0' + low : 'a' + low - 10);
+    *hi = (char)(high < 10 ? '0' + high : 'a' + high - 10);
 }
 
-int rgb(int r, int g, int b, char* output) {
-    r = aprx(r);
-    g = aprx(g);
-    b = aprx(b);
-
-    dec_to_hex(b, &output[0]);
-    dec_to_hex(g, &output[0]);
-    dec_to_hex(r, &output[0]);
-
+static int rgb(u8 r, u8 g, u8 b, char* const output) {
+    dec_to_hex(b, &output[5], &output[4]);
+    dec_to_hex(g, &output[3], &output[2]);
+    dec_to_hex(r, &output[1], &output[0]);
     return 0;
 }
 
 int main() {
-
     char buffer[7] = { ' ', ' ', ' ', ' ', ' ', ' ', '\0' };
-
-    pointer = 5;
-
-    rgb(12, 33, 18, &buffer[0]);
-
-    printf("0x%s\n", buffer);
-
-    // cout<< buffer << "\n";
+    rgb(12, 33, 18, buffer);
+    printf("rgb(12,33,18) = #%s\n", buffer);
     return 0;
 }
