52eaad224ac68402f7dfc8fc3540cfd568ae318c
default-writer <funcelot@outlook.com>
2023-02-05 13:32:43 +0300

    [master] code simplifications

--- tests/main-brain.c
+++ tests/main-brain.c
@@ -6,10 +6,6 @@ extern struct pointer_methods pointer_methods_definition;
 
 const struct pointer_methods* pointer = &pointer_methods_definition;
 
-void open_file();
-void read_file();
-void process();
-
 extern inline struct pointer* get_full_path() {
     struct pointer* argv_ptr = pointer->pop(); // NOLINT
     struct pointer* data_ptr = pointer->alloc(PATH_MAX);
@@ -30,28 +26,6 @@ struct data {
     char* file;
 };
 
-void open_file() {
-    struct pointer* file_path_ptr = pointer->pop();
-    struct pointer* mode_ptr = pointer->pop();
-    struct pointer* f_ptr = pointer->open_file(file_path_ptr, mode_ptr);
-    pointer->free(mode_ptr);
-    pointer->free(file_path_ptr);
-    pointer->push(f_ptr);
-}
-
-void read_file() {
-    struct pointer* f_ptr = pointer->pop();
-    struct pointer* data_ptr = pointer->data(f_ptr);
-    pointer->free(f_ptr);
-    pointer->push(data_ptr);
-}
-
-extern inline void process() {
-    struct pointer* data_ptr = pointer->pop();
-    pointer->printf(data_ptr);
-    pointer->free(data_ptr);
-}
-
 int main(int argc, char** argv) {
     if (argc > 0) {
         pointer_init();
@@ -59,13 +33,15 @@ int main(int argc, char** argv) {
         struct pointer* argv_ptr = pointer->load(argv[0]);
         pointer->push(file_name_ptr);
         pointer->push(argv_ptr);
-        struct pointer* cwd_ptr = get_full_path();
+        struct pointer* file_path_ptr = get_full_path();
         struct pointer* mode_ptr = pointer->load("rb");
-        pointer->push(mode_ptr);
-        pointer->push(cwd_ptr);
-        open_file();
-        read_file();
-        process();
+        struct pointer* f_ptr = pointer->open_file(file_path_ptr, mode_ptr);
+        pointer->free(mode_ptr);
+        pointer->free(file_path_ptr);
+        struct pointer* data_ptr = pointer->data(f_ptr);
+        pointer->free(f_ptr);
+        pointer->printf(data_ptr);
+        pointer->free(data_ptr);
         pointer_destroy();
     }
     return 0;
