3daba9c6634b750fe699a363cc59c79c66e9f54a
default-writer <funcelot@outlook.com>
2023-02-12 09:58:11 +0300

    [virtual_address] added virtual memory prototype

---
+++ src/playground/virtual/vm.c
@@ -0,0 +1,109 @@
+#include "playground/virtual/vm.h"
+#include "playground/list/v2/list.h"
+
+#define DEFAULT_SIZE 0xffff // 64K bytes
+
+/* list definition */
+extern const struct list list_v2;
+
+/*private */
+static const struct list* list = &list_v2;
+static struct list_data* cache;
+
+/* api */
+
+static struct vm_data* vm_init(u64 size);
+static void vm_destroy(struct vm_data* pointer);
+u64 vm_alloc(struct vm_data* pointer);
+void vm_free(struct vm_data* pointer, u64 address);
+static void* vm_read(struct vm_data* pointer, u64 address);
+static void vm_write(struct vm_data* pointer, u64 address, void* value);
+
+/* internal */
+
+static void* to_real_address(void** base, u64 address);
+static u64 to_virtual_address(void** base, void** ptr);
+
+/* implementation */
+
+static struct vm_data* vm_init(u64 size) {
+    struct vm_data* pointer = calloc(1, sizeof(struct vm_data));
+    pointer->base = calloc(1, size * sizeof(void*));
+    void* max = (void*)((u64*)pointer->base + size);
+    pointer->max = max;
+    pointer->size = size;
+    pointer->ptr = pointer->base;
+    cache = list->alloc(DEFAULT_SIZE);
+    return pointer;
+}
+
+static void vm_destroy(struct vm_data* pointer) {
+    list->free(cache);
+    cache = 0;
+    free(pointer->base);
+#ifdef USE_MEMORY_CLEANUP
+    pointer->base = 0;
+    pointer->ptr = 0;
+    pointer->max = 0;
+    pointer->size = 0;
+#endif
+    free(pointer);
+    pointer = 0;
+}
+
+u64 vm_alloc(struct vm_data* pointer) {
+    u64 offset = 0;
+    void* ptr = list->pop(cache);
+    if (ptr == 0) {
+        ptr = pointer->ptr++;
+    }
+    if (ptr != pointer->max) {
+        offset = to_virtual_address(pointer->base, ptr);
+    }
+    return offset + 1;
+}
+
+void vm_free(struct vm_data* pointer, u64 address) {
+    if (address > 0 && address < pointer->size) {
+        void** ptr = to_real_address(pointer->base, address);
+        if (*ptr != 0) {
+            list->push(cache, ptr);
+            *ptr = 0;
+        }
+    }
+}
+
+static u64 to_virtual_address(void** base, void** ptr) {
+    return (u64)(ptr - base) + 1;
+}
+
+static void* to_real_address(void** base, u64 address) {
+    return base + address - 1;
+}
+
+static void* vm_read(struct vm_data* pointer, u64 address) {
+    void** data = 0;
+    if (address > 0 && address < pointer->size) {
+        data = to_real_address(pointer->base, address);
+        data = *data;
+    }
+    return data;
+}
+
+static void vm_write(struct vm_data* pointer, u64 address, void* value) {
+    if (address > 0 && address < pointer->size) {
+        void** ptr = to_real_address(pointer->base, address);
+        *ptr = value;
+    }
+}
+
+/* public */
+
+const struct vm vm_definition = {
+    .init = vm_init,
+    .destroy = vm_destroy,
+    .alloc = vm_alloc,
+    .free = vm_free,
+    .read = vm_read,
+    .write = vm_write
+};
--- tests/main-brain.c
+++ tests/main-brain.c
@@ -1,29 +1,187 @@
-#include "playground/pointer/pointer.h"
+#include "common/alloc.h"
 
 #include "playground/brain/brain.h"
-#include <string.h>
+#include "playground/list/v2/list.h"
+#include "playground/pointer/pointer.h"
+#include "playground/virtual/vm.h"
+
+#include "rexo/include/rexo.h"
+
+#define DEFAULT_SIZE 0xffff // 64K bytes
+
+/* list definition */
+extern const struct vm vm_definition;
+extern const struct list list_v2;
+
+/* list definition */
+static const struct list* list = &list_v2;
+static const struct vm* vm = &vm_definition;
 
 extern struct pointer_methods pointer_methods_definition;
 
 const struct pointer_methods* pointer = &pointer_methods_definition;
 
+typedef struct test_data {
+    struct init_data* ctx;
+}* TEST_DATA;
+
+RX_SET_UP(test_set_up) {
+    TEST_DATA rx = (TEST_DATA)RX_DATA;
+    struct init_data** ctx = &rx->ctx;
+    *ctx = calloc(1, sizeof(struct init_data));
+    struct init_data* init = *ctx;
+    init->vm = vm->init(DEFAULT_SIZE);
+    init->list = list->alloc(DEFAULT_SIZE);
+#ifdef USE_GC
+    init->gc_list = list->alloc(DEFAULT_SIZE);
+#endif
+    pointer_set(*ctx);
+    return RX_SUCCESS;
+}
+
+RX_TEAR_DOWN(test_tear_down) {
+    TEST_DATA rx = (TEST_DATA)RX_DATA;
+    struct init_data** ctx = &rx->ctx;
+    struct init_data* init = *ctx;
+#ifdef USE_GC
+    pointer->gc();
+#endif
+    pointer_get(init);
+    vm->destroy(init->vm);
+    list->free(init->list);
+#ifdef USE_GC
+    list->free(init->gc_list);
+#endif
+    free(*ctx);
+}
+
+/* Define the fixture. */
+RX_FIXTURE(test_fixture, TEST_DATA, .set_up = test_set_up, .tear_down = test_tear_down);
+
+// test init
+RX_TEST_CASE(myTestSuite, test_print_0, .fixture = test_fixture) {
+    pointer->printf(0);
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_load_0, .fixture = test_fixture) {
+    u64 ptr = pointer->load(0);
+    RX_ASSERT(ptr == 0);
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_load_1, .fixture = test_fixture) {
+    u64 ptr = pointer->load("");
+    RX_ASSERT(ptr == 0);
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_print_load_1, .fixture = test_fixture) {
+    u64 empty_ptr = pointer->load("");
+    pointer->printf(empty_ptr);
+    RX_ASSERT(empty_ptr == 0);
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_strcpy_0, .fixture = test_fixture) {
+    u64 pattern_ptr = pointer->load("/");
+    u64 empty_ptr = pointer->alloc(0);
+    pointer->strcpy(empty_ptr, pattern_ptr);
+    RX_ASSERT(pattern_ptr != 0);
+    RX_ASSERT(empty_ptr != 0);
+#ifndef USE_GC
+    pointer->free(empty_ptr);
+    pointer->free(pattern_ptr);
+#endif
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_strcat_0, .fixture = test_fixture) {
+    u64 zero_ptr = pointer->alloc(0);
+    u64 char_ptr = pointer->load("/");
+    pointer->strcat(zero_ptr, char_ptr);
+    RX_ASSERT(zero_ptr != 0);
+    RX_ASSERT(char_ptr != 0);
+#ifndef USE_GC
+    pointer->free(zero_ptr);
+    pointer->free(char_ptr);
+#endif
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_strcat_1, .fixture = test_fixture) {
+    u64 char_ptr = pointer->load("/");
+    u64 pattern_ptr = pointer->alloc(1);
+    pointer->strcat(pattern_ptr, char_ptr);
+    RX_ASSERT(char_ptr != 0);
+    RX_ASSERT(pattern_ptr != 0);
+#ifndef USE_GC
+    pointer->free(char_ptr);
+    pointer->free(pattern_ptr);
+#endif
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_load_copy, .fixture = test_fixture) {
+    u64 char_ptr = pointer->load("/");
+    u64 copy_ptr = pointer->copy(char_ptr);
+    RX_ASSERT(char_ptr != 0);
+    RX_ASSERT(copy_ptr != 0);
+    RX_ASSERT(char_ptr != copy_ptr);
+#ifndef USE_GC
+    pointer->free(char_ptr);
+    pointer->free(copy_ptr);
+#endif
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_push_peek_pop, .fixture = test_fixture) {
+    u64 char_ptr = pointer->load("/");
+    pointer->push(char_ptr);
+    u64 peek_ptr = pointer->peek();
+    u64 pop_ptr = pointer->pop();
+    RX_ASSERT(char_ptr != 0);
+    RX_ASSERT(peek_ptr != 0);
+    RX_ASSERT(pop_ptr != 0);
+#ifndef USE_GC
+    pointer->free(char_ptr);
+    pointer->free(peek_ptr);
+    pointer->free(pop_ptr);
+#endif
+}
+
+RX_TEST_CASE(myTestSuite, test_double_free, .fixture = test_fixture) {
+    u64 char_ptr = pointer->load("/");
+    RX_ASSERT(char_ptr != 0);
+#ifndef USE_GC
+    pointer->free(char_ptr);
+    pointer->free(char_ptr);
+#endif
+}
+
+// test init
+RX_TEST_CASE(myTestSuite, test_strcpy_load, .fixture = test_fixture) {
+    u64 char_ptr = pointer->load("/");
+    u64 pattern_ptr = pointer->alloc(1);
+    u64 empty_ptr = pointer->alloc(0);
+    pointer->strcat(pattern_ptr, char_ptr);
+#ifndef USE_GC
+    pointer->free(char_ptr);
+#endif
+    pointer->strcpy(empty_ptr, pattern_ptr);
+    RX_ASSERT(pattern_ptr != 0);
+    RX_ASSERT(empty_ptr != 0);
+#ifndef USE_GC
+    pointer->free(empty_ptr);
+    pointer->free(pattern_ptr);
+#endif
+}
+
 extern inline void source() {
-    struct pointer* file_name_ptr = pointer->load("/input.txt");
-    struct pointer* head_ptr = pointer->peek();
-    pointer->printf(head_ptr);
-    struct pointer* argc_ptr = pointer->pop(); // NOLINT
-    ZEROPTR(argc_ptr)
-#ifndef USE_GC
-    pointer->free(argc_ptr);
-#endif
-    struct pointer* argv_ptr = pointer->pop();
-    struct pointer* data_ptr = pointer->alloc(PATH_MAX);
-    pointer->strcpy(data_ptr, argv_ptr);
-#ifndef USE_GC
-    pointer->free(argv_ptr);
-#endif
-    struct pointer* pattern_ptr = pointer->load("/");
-    struct pointer* last_match_ptr = pointer->match_last(data_ptr, pattern_ptr);
+    u64 data_ptr = pointer->pop();
+    u64 file_name_ptr = pointer->load("/input.txt");
+    u64 pattern_ptr = pointer->load("/");
+    u64 last_match_ptr = pointer->match_last(data_ptr, pattern_ptr);
 #ifndef USE_GC
     pointer->free(pattern_ptr);
 #endif
@@ -35,13 +193,13 @@ extern inline void source() {
 #ifndef USE_GC
     pointer->free(file_name_ptr);
 #endif
-    struct pointer* mode_ptr = pointer->load("rb");
-    struct pointer* f_ptr = pointer->open_file(data_ptr, mode_ptr);
+    u64 mode_ptr = pointer->load("rb");
+    u64 f_ptr = pointer->open_file(data_ptr, mode_ptr);
 #ifndef USE_GC
     pointer->free(data_ptr);
     pointer->free(mode_ptr);
 #endif
-    struct pointer* file_data_ptr = pointer->read_file(f_ptr);
+    u64 file_data_ptr = pointer->read_file(f_ptr);
     pointer->close_file(f_ptr);
 #ifndef USE_GC
     pointer->free(f_ptr);
@@ -53,16 +211,21 @@ extern inline void source() {
 }
 
 int main(int argc, char** argv) {
-    pointer_init();
-    struct pointer* argv_ptr = pointer->load(argv[0]);
-    pointer->push(argv_ptr);
-    char buffer[24];
-    memset(buffer, 0, 24); // NOLINT
-    sprintf(buffer, "%d", argc); // NOLINT
     ZEROPTR(argc)
-    struct pointer* argc_ptr = pointer->load(buffer);
-    pointer->push(argc_ptr);
+#ifdef USE_MEMORY_DEBUG_INFO
+    printf("---- acceptance test code\n");
+#endif
+    pointer_init();
+    u64 argv_ptr = pointer->load(argv[0]);
+    pointer->push(argv_ptr);
     source();
+#ifdef USE_GC
+    pointer->gc();
+#endif
     pointer_destroy();
-    return 0;
+#ifdef USE_MEMORY_DEBUG_INFO
+    printf("---- rexo unit test code\n");
+#endif
+    /* Execute the main function that runs the test cases found. */
+    return rx_run(0, NULL) == RX_SUCCESS ? 0 : 1;
 }
