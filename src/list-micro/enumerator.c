009864be94a958a4db8bd1b80693efe71d4b2693
default-writer <funcelot@outlook.com>
2023-02-24 10:28:55 +0300

    [master] added sample list-micro enumerator

---
+++ src/list-micro/enumerator.c
@@ -0,0 +1,36 @@
+#include "common/alloc.h"
+
+#include "std/enumerator.h"
+
+#include "list-micro/data.h"
+
+struct list_data_enumerator {
+    struct enumerator_data* (*init)(void);
+    void (*destroy)(struct enumerator_data* enumerator);
+    void* (*next)(struct enumerator_data* ptr);
+};
+
+static void* list_data_enumerator_next(struct enumerator_data* enumerator) {
+    struct list_data* list = (struct list_data*)enumerator->current;
+    void* data = 0;
+    if (list != 0) {
+        enumerator->current = (void*)list->next;
+        data = list->data;
+    }
+    enumerator->value = data;
+    return enumerator->value;
+}
+
+static struct enumerator_data* list_enumerator_init(void) {
+    return _list_alloc(sizeof(struct enumerator_data));
+}
+
+static void list_enumerator_destroy(struct enumerator_data* enumerator) {
+    _list_free(enumerator, sizeof(struct enumerator_data));
+}
+
+const struct list_data_enumerator list_data_enumerator_definition = {
+    .init = list_enumerator_init,
+    .destroy = list_enumerator_destroy,
+    .next = list_data_enumerator_next
+};
