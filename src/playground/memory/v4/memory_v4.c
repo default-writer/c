56051585e8ab8ff72b95694f99fbd30630dd6dc1
default-writer <funcelot@outlook.com>
2023-02-28 22:24:25 +0300

    [master] new ideas

--- src/playground/memory/v4/memory_v4.c
+++ src/playground/memory/v4/memory_v4.c
@@ -6,6 +6,54 @@
 
 #include "list-micro/data.h"
 
+// the idea:
+//
+// minimal allocation unit - 64 bit (8-byte block)
+//
+// allocation layers (32)
+//
+// layer-0 1 8-byte blocks
+// layer-1 2 8-byte blocks
+// layer-2 4 8-byte blocks
+// layer-3 8 8-byte blocks
+// layer-4 16 8-byte blocks
+// ...
+// layer-n pow(2,n) 8-byte blocks
+// ...
+// layer-31 2^32 8-byte blocks (32 GB)
+//
+// practically, it is enough to have 16 levels, 2^16*8 = 524288 bytes
+// at worst case, algorithm will lose 50% memory for allocating first 1 MB of data, then, it can't be worse than that
+// for a sequence of equal memory block size. i.e.
+//
+//  blocks - loses
+//  1 bytes in 1 byte blocks - 0%
+//  2 bytes in 2 byte blocks - 0%
+//  3 bytes in 4 byte blocks - 25%
+//  4 bytes in 4 byte blocks - 0%
+//  5 bytes in 8 byte blocks - 37.5%
+//  6 bytes in 8 byte blocks - 25%
+//  7 bytes in 8 byte blocks - 12.5%
+//  8 bytes in 8 byte blocks - 0%
+//
+// but if we pass the rest to the cache to direct level below, that will virtually eliminate some space losses, if used
+//
+// for example, allocating 9 bytes will allocate two 8 byte blocks, by splitting second block to 1 and (1+2+4) block
+//
+// each layer has own block cache
+// if needed, lower layer can allocate in one of upper level blocks depending or number of blocks
+//
+// when freeing blocks, if 2 sequential blocks forms the bigger block of size for one layer up,
+// it will moves to upper level cache
+//
+// when allocating blocks, if current level blocks cache is empty, we will go to one level up
+// after allocation of upper level block, it will move from used on upper level to cached block
+// on level below, so it can be utilize by memory allocator on level below allocation level
+//
+// basically,
+// allocating blocks will move down on the allocation level and split (allocated lvl -> cached lvl-1)
+// freeing blocks will join the neighbour to the right and move up on the allocation levels (freed lvl -> cached lvl+1)
+
 /* list definition */
 extern const struct list list_micro_definition;
 
@@ -29,13 +77,12 @@ static void* memory_alloc_internal(u64 size) {
     void** next = current - 1;
     void* prev = current;
 
-    void** tmp = _list_alloc((size + 3) * sizeof(void*));
+    void** tmp = _list_alloc((size + 2) * sizeof(void*));
     next = *next;
     *next = tmp;
 
     *tmp = prev;
-    *(tmp + 1) = tmp + size + 2;
-    *(tmp + 2) = next;
+    *(tmp + 1) = tmp + size + 1;
 
     current = tmp + 2;
 
@@ -47,7 +94,7 @@ static u64 offset(void* data) {
     if (data != 0) {
         void** head = data;
         void** next = *(head - 1);
-        size = (u64)(next - head);
+        size = (u64)(next - head) + 1;
     }
     return size;
 }
@@ -63,7 +110,6 @@ static void memory_init(void) {
 
     *tmp = current;
     *(tmp + 1) = tmp + 2;
-    *(tmp + 2) = next;
 
     current = tmp + 2;
 
@@ -106,15 +152,7 @@ static void* memory_alloc(u64 size) {
 
 // releases global memory
 static void memory_free(void* data) {
-    void** head = data;
-    void** next = *(head - 1);
-    void** last = *(head - 2);
-    if (*next == 0) {
-        list->push(cache, data);
-        void** prev = last;
-        next = prev + offset(prev);
-        *next = 0;
-    }
+    list->push(cache, data);
 }
 
 // releases global memory
@@ -126,7 +164,7 @@ static void memory_free_internal(void* data) {
     printf("  0-: 0x%016llx !  %16lld\n", (u64)last, size);
 #endif
 #ifdef USE_MEMORY_CLEANUP
-    memset(head - 2, 0, (size + 3) * sizeof(void*)); // NOLINT
+    memset(head - 2, 0, (size + 2) * sizeof(void*)); // NOLINT
 #endif
     _list_free(head - 2, size);
 }
