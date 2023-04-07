45def7083e302b9b446d4ac5329623d1d75a7f0c
default-writer <funcelot@devweb.app>
2023-04-07 15:19:52 +0000

    [master] added lcob install

--- bin/utils/setup.sh
+++ bin/utils/setup.sh
@@ -32,6 +32,7 @@ case "${install}" in
         dpkg --configure -a
         DEBIAN_FRONTEND=noninteractive apt-get install -y keyboard-configuration gettext-base
         update
+        apt -y install --no-install-recommends curl ca-certificates git build-essential lldb lcov cmake clangd g++ gcc gdb lcov ninja-build
         upgrade
         ;;
 
