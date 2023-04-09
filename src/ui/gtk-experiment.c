1b662165660eee350de3101963db7bfd2bd91aa6
default-writer <funcelot@outlook.com>
2023-04-09 21:37:36 +0300

    [master] reorder in cmake

--- src/ui/gtk-experiment.c
+++ src/ui/gtk-experiment.c
@@ -1,12 +1,10 @@
 #include <gtk/gtk.h>
 
-static void print_hello(GtkWidget* widget,
-    gpointer data) {
+static void print_hello(GtkWidget* widget, gpointer data) {
     g_print("Hello World\n");
 }
 
-static void activate(GtkApplication* app,
-    gpointer user_data) {
+static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget* window;
     GtkWidget* button;
 
