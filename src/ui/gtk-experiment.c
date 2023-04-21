b9a6a422253a24bd1f56f3c46d70d7ebc36d6387
default-writer <funcelot@outlook.com>
2023-04-21 22:04:00 +0300

    [master] updates using format --all

--- src/ui/gtk-experiment.c
+++ src/ui/gtk-experiment.c
@@ -25,8 +25,8 @@ int main(int argc, char** argv) {
     if (ui_mode != 0 && strcmp(ui_mode, "--ui") == 0) {
         GtkApplication* app;
         app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-        g_application_add_main_option (G_APPLICATION (app), "ui", 0, 0, G_OPTION_ARG_NONE, "Start GUI", NULL);
-        g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);        
+        g_application_add_main_option(G_APPLICATION(app), "ui", 0, 0, G_OPTION_ARG_NONE, "Start GUI", NULL);
+        g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
         status = g_application_run(G_APPLICATION(app), argc, argv);
         g_object_unref(app);
     }
