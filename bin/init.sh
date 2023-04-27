e554ff2ed4b1b796b88866ae25ce829db111fd06
default-writer <funcelot@outlook.com>
2023-04-27 19:31:28 +0300

    [master] cleaned up init

--- bin/init.sh
+++ bin/init.sh
@@ -24,19 +24,6 @@ sudo "${pwd}/bin/utils/setup.sh" --configuration
 sudo "${pwd}/bin/utils/setup.sh" --clang-format
 sudo "${pwd}/bin/utils/setup.sh" --cmake
 sudo "${pwd}/bin/utils/setup.sh" --gtk4
-sudo "${pwd}/bin/utils/setup.sh" --llvm
-
-# "${pwd}/bin/utils/env.sh" --nvm
-# "${pwd}/bin/utils/nvm.sh"
-
-# . "${pwd}/bin/utils/nvm.sh"
-
-# nvm --version
-# nvm install 19.3.0
-# nvm use 19.3.0
-# npm install -g npm@9.2.0
-# echo "Node version $(node --version)"
-# echo "NPM version $(npm --version)"
 
 "${pwd}/bin/utils/runme.sh"
 
