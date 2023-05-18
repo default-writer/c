39bb3528c30c86633d1a6132931e049a7fb63625
default-writer <funcelot@outlook.com>
2023-05-18 15:15:20 +0300

    [master] refactoring gc internals

--- src/common/alloc.c
+++ src/common/alloc.c
@@ -5,8 +5,8 @@
 #endif
 
 #ifdef USE_MEMORY_DEBUG_INFO
-static u64 global_allocated = 0;
-static u64 global_freed = 0;
+static u64 _gc_alloc = 0;
+static u64 _gc_free = 0;
 #endif
 
 void* _list_alloc(u64 size) {
@@ -18,8 +18,8 @@ void* _list_alloc(u64 size) {
         ptr = calloc(1, size);
 #endif
 #ifdef USE_MEMORY_DEBUG_INFO
-        global_allocated += size;
-        printf("   +: 0x%016llx !  %16lld .  %16lld :  %16lld\n", (u64)ptr, size, global_freed, global_allocated);
+        _gc_alloc += size;
+        printf("   +: 0x%016llx !  %16lld .  %16lld :  %16lld\n", (u64)ptr, size, _gc_free, _gc_alloc);
 #endif
     }
     return ptr;
@@ -28,8 +28,8 @@ void* _list_alloc(u64 size) {
 void _list_free(void* ptr, u64 size) {
     if (ptr != 0) {
 #ifdef USE_MEMORY_DEBUG_INFO
-        global_freed += size;
-        printf("   -: 0x%016llx !  %16lld .  %16lld :  %16lld\n", (u64)ptr, size, global_freed, global_allocated);
+        _gc_free += size;
+        printf("   -: 0x%016llx !  %16lld .  %16lld :  %16lld\n", (u64)ptr, size, _gc_free, _gc_alloc);
 #endif
 #ifdef USE_MEMORY_CLEANUP
         memset((void*)(u8*)ptr, 0, size); /* NOLINT */
@@ -46,7 +46,7 @@ void* _list_realloc(void* ptr, u64 size, u64 new_size) {
     void* data = 0;
     if (ptr != 0 && new_size > size) {
 #ifdef USE_MEMORY_DEBUG_INFO
-        global_allocated += new_size - size;
+        _gc_alloc += new_size - size;
 #endif
         data = realloc(ptr, new_size);
     }
