f1cceb81d136fb9949b802627af6284f241cfa72
default-writer <funcelot@outlook.com>
2023-07-05 10:38:42 +0300

    [master] cleanup comments

--- src/stl/linked_list.h
+++ src/stl/linked_list.h
@@ -2,14 +2,11 @@
 #define _STL_LINKED_LIST_H_
 
 struct linked_list {
-    /* points to previous node */
     struct linked_list* next;
-    /* data */
     void* data;
 };
 
 struct linked_list_methods {
-    /* initialize context */
     struct linked_list* (*new)(void);
     void (*delete)(struct linked_list* head);
     struct linked_list* (*reverse_list)(struct linked_list* head);
