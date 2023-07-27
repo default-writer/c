7fda10622ed129cb1b21e47cda26002334aff7e6
default-writer <funcelot@outlook.com>
2023-07-27 09:46:52 +0300

    [code-coverage-compilation-scripts] updated cmake targets

--- bin/cmake.sh
+++ bin/cmake.sh
@@ -123,21 +123,56 @@ fi
 cmake=$(get-cmake)
 targets=( $(get-cmake-targets) )
 
-if [ "${target}" == "--target" ]; then
-    for target in ${targets[@]}; do
-        if [ "${target}" == "$2" ]; then 
-            array=( ${target} )
-            break
+found_target=false
+for target in ${targets[@]}; do
+    if [ "${target}" == "$2" ]; then 
+        array=( ${target} )
+        found_target=true
+    fi
+done
+
+if [[ "${found_target}" == false ]]; then
+    array=()
+    target=$2
+    found_target_base=false
+    sources=$(find "${pwd}/config" -type f -name "sources.txt")
+    for source in ${sources[@]}; do
+        # Find target directory for the given source file
+        while IFS= read -r line; do
+            if [[ "${target}" == "${line}" ]]; then
+                found_target_base=true
+                target_base=$(basename $(dirname "${source}"))
+                array+=( ${target_base} )
+                break
+            fi
+        done <  $source
+        if [[ "${found_target_base}" == true ]]; then
+            linked_targets=$(get-linked-targets ${target_base})
+            if [ "${target}" == "--target ${line}" ]; then
+                for linked_target in ${linked_targets[@]}; do
+                    if [ "${linked_target}" == "$2" ]; then 
+                        array+=( ${linked_target} )
+                    fi
+                done
+            fi
         fi
     done
-    if [ "$(echo ${array[@]})" == "" ]; then
-        if [[ "${help}" == "--help" ]]; then
-            help
-        fi
-        echo ERROR
-        exit 8
+fi
+
+targets=( ${array[@]} )
+
+if [[ "${target_base}" == "" ]]; then
+    target_base="${targets[@]}"
+fi
+
+targets=$(echo "${targets[@]} ${linked_targets[@]}" | xargs -n1 | sort -u | xargs)
+
+if [[ "${targets[@]}" == "" ]]; then
+    if [[ "${help}" == "--help" ]]; then
+        help
     fi
-    targets=( ${array[@]} )
+    echo ERROR
+    exit 8
 fi
 
 export MAKEFLAGS=-j8
