4fd09d3dbe12c977aa10dbe9fd78dee37cce74b9
default-writer <funcelot@outlook.com>
2023-08-12 09:08:47 +0300

    [master] added musl confugurator

---
+++ bin/configure.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -e
+
+err_report() {
+    echo "ERROR: on line $*: $(cat $0 | sed $1!d)" >&2
+}
+
+trap 'err_report $LINENO' ERR
+
+uid=$(id -u)
+
+pwd=$(pwd)
+
+pwd=$(pwd)
+
+install="$1"
+
+opts=( "${@:2}" )
+
+. "${pwd}/bin/scripts/load.sh"
+
+## Builds binaries and creates coverage info
+## Usage: ${script} <option> [optional]
+## ${commands}
+
+case "${install}" in
+
+    "")
+        source="all"
+        ;;
+
+    "--target") # builds and runs specified target
+        source="$2"
+        opts=( "${@:3}" )
+        ;;
+
+    "--all") # builds and runs specified target
+        source="all"
+        opts=( "${@:2}" )
+        ;;
+
+    *)
+        help
+        ;;
+
+esac
+
+for opt in ${opts[@]}; do
+    case ${opt} in
+
+        "")
+            ;;
+
+        "--silent") # [optional] suppress verbose output
+            silent="--silent"
+            ;;
+
+        "--help") # [optional] shows command desctiption
+            help
+            ;;
+
+        *)
+            help
+            ;;
+
+    esac
+done
+
+if [[ "${musl}" == "--musl" ]]; then
+    cd "${pwd}/.deps/musl"
+    ./configure
+fi
+
+if [ "${silent}" == "--silent" ]; then
+    exec 1>&2 2>&-
+fi
+
+[[ $SHLVL -gt 2 ]] || echo OK
+
+cd "${pwd}"
