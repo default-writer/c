10bcf080c23ec83eb6ba03a55f97bfb70db817a9
default-writer <funcelot@outlook.com>
2023-08-17 11:38:01 +0300

    [master] fix build script

--- bin/src.sh
+++ bin/src.sh
@@ -140,7 +140,7 @@ for linked_target in ${targets[@]}; do
     case ${linked_target} in
         c-*) ;& main-*) ;& test-*)
             if [[ " ${targets[*]} " == *" ${linked_target} "* ]]; then
-                ${pwd}/bin/utils/coverageall.sh --target ${linked_target} ${opts[@]}
+                ${pwd}/bin/utils/coverage.sh --target ${linked_target} ${opts[@]}
             fi
             ;;
         *)
