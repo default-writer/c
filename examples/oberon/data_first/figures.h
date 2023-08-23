a12be0a6e6433b7a75fe44b889349c9fd2f1ab6c
default-writer <funcelot@outlook.com>
2023-08-23 18:52:19 +0300

    [master] updated codebase and workspace

---
+++ examples/oberon/data_first/figures.h
@@ -0,0 +1,15 @@
+#ifndef _FIGURES_H_
+#define _FIGURES_H_
+
+typedef struct figure* figure_pointer;
+
+typedef struct figure_data {
+    int version;
+} *figure_data_pointer, figure_data;
+
+typedef struct figure_type {
+    void (*draw)(figure_data_pointer f);
+    void (*move)(figure_data_pointer f, int dx, int dy);
+} *figure_type_pointer, figure_type;
+
+#endif // _FIGURES_H_
