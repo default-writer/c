fe6e842eac56083166d64a1e28761fc3b575e45a
defaut-writer <funcelot@outlook.com>
2023-09-10 01:48:07 +0300

    [master] simplified parser example

--- tutorials/src/simple-parser/main.c
+++ tutorials/src/simple-parser/main.c
@@ -8,7 +8,6 @@ typedef void (*function)(func_struct_pointer self);
 static void _a(func_struct_pointer self);
 static void _b(func_struct_pointer self);
 static void _c(func_struct_pointer self);
-static void _d(func_struct_pointer self);
 
 struct func_struct {
     char* input;
@@ -17,7 +16,6 @@ struct func_struct {
     const function _a;
     const function _b;
     const function _c;
-    const function _d;
 };
 
 static func_struct finite_automata = {
@@ -26,8 +24,7 @@ static func_struct finite_automata = {
     ._ = 0,
     ._a = _a,
     ._b = _b,
-    ._c = _c,
-    ._d = _d
+    ._c = _c
 };
 
 static void _a(func_struct_pointer self) {
@@ -35,7 +32,7 @@ static void _a(func_struct_pointer self) {
         self->_ = self->_b;
         self->input++;
     } else {
-        self->_ = self->_d;
+        self->_ = 0;
     }
 }
 
@@ -44,21 +41,17 @@ static void _b(func_struct_pointer self) {
         self->_ = self->_c;
         self->input++;
     } else {
-        self->_ = self->_d;
+        self->_ = 0;
     }
 }
 
 static void _c(func_struct_pointer self) {
     if (*self->input == '\0') {
         self->result = 1;
-        self->_ = self->_d;
+        self->_ = 0;
     } else {
         self->_ = self->_a;
     }
-}
-
-static void _d(func_struct_pointer self) {
-    self->_ = 0;
 }
 
 static void read_input(func_struct_pointer self) {
