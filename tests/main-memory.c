7f794d1d013aed3c807a4cabe560751bf62c4b43
default-writer <funcelot@outlook.com>
2023-11-24 01:47:45 +0300

    [master] unified memory api versioning tests

--- tests/main-memory.c
+++ tests/main-memory.c
@@ -26,49 +26,86 @@
 #include "common/memory.h"
 #include "playground/memory/api/memory.h"
 
-extern const struct memory_allocator_v2 memory_allocator_v4;
+extern const union memory_allocator_api memory_allocator_v1;
+extern const union memory_allocator_api memory_allocator_v2;
+extern const union memory_allocator_api memory_allocator_v3;
+extern const union memory_allocator_api memory_allocator_v4;
 
-void use(const struct memory_allocator_v2* allocator) {
+void use_v1(const union memory_allocator_api* allocator) {
     /* initializes memory pool */
-    allocator->init();
+    allocator->v1.init();
     /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
-    u64 size1 = 2;
+    u64 size = 2;
     /* allocates memory block */
-    void** ptr1 = allocator->alloc(size1);
-    for (u64 i = 0; i < size1; i++) {
-        *(ptr1 + i) = (void*)0x7bde84217bde8421;
+    void** ptr = allocator->v1.alloc(size);
+    for (u64 i = 0; i < size; i++) {
+        *(ptr + i) = (void*)0x7bde84217bde8421;
     }
     /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
     u64 size2 = 3;
     /* allocates memory block */
-    void** ptr2 = allocator->alloc(size2);
+    void** ptr2 = allocator->v1.alloc(size2);
     for (u64 i = 0; i < size2; i++) {
         *(ptr2 + i) = (void*)0x7bde84217bde8421;
     }
     /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
     u64 size3 = 16;
     /* allocates memory block */
-    void** ptr3 = allocator->alloc(size3);
+    void** ptr3 = allocator->v1.alloc(size3);
     for (u64 i = 0; i < size3; i++) {
         *(ptr3 + i) = (void*)0x7bde84217bde8421;
     }
     /* releases memory block */
-    allocator->free(ptr3);
+    allocator->v1.free(ptr3, size3);
+    /* releases memory block */
+    allocator->v1.free(ptr2, size2);
+    /* releases memory block */
+    allocator->v1.free(ptr, size);
+    /* destroys memory pool */
+    allocator->v1.destroy();
+}
+
+void use_v2(const union memory_allocator_api* allocator) {
+    /* initializes memory pool */
+    allocator->v2.init();
+    /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
+    u64 size1 = 2;
+    /* allocates memory block */
+    void** ptr1 = allocator->v2.alloc(size1);
+    for (u64 i = 0; i < size1; i++) {
+        *(ptr1 + i) = (void*)0x7bde84217bde8421;
+    }
+    /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
+    u64 size2 = 3;
+    /* allocates memory block */
+    void** ptr2 = allocator->v2.alloc(size2);
+    for (u64 i = 0; i < size2; i++) {
+        *(ptr2 + i) = (void*)0x7bde84217bde8421;
+    }
+    /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
+    u64 size3 = 16;
+    /* allocates memory block */
+    void** ptr3 = allocator->v2.alloc(size3);
+    for (u64 i = 0; i < size3; i++) {
+        *(ptr3 + i) = (void*)0x7bde84217bde8421;
+    }
+    /* releases memory block */
+    allocator->v2.free(ptr3);
     /* allocation size aligned to 8 byte boundaries (64-bit pointers) */
     u64 size4 = 16;
     /* allocates memory block */
-    void** ptr4 = allocator->alloc(size4);
+    void** ptr4 = allocator->v2.alloc(size4);
     for (u64 i = 0; i < size4; i++) {
         *(ptr4 + i) = (void*)0x7bde84217bde8421;
     }
     /* releases memory block */
-    allocator->free(ptr4);
+    allocator->v2.free(ptr4);
     /* releases memory block */
-    allocator->free(ptr2);
+    allocator->v2.free(ptr2);
     /* releases memory block */
-    allocator->free(ptr1);
+    allocator->v2.free(ptr1);
     /* destroys memory pool */
-    allocator->destroy();
+    allocator->v2.destroy();
 }
 
 int main(void) {
@@ -77,7 +114,10 @@ int main(void) {
     global_statistics();
 #endif
 #endif
-    use(&memory_allocator_v4);
+    use_v1(&memory_allocator_v1);
+    use_v1(&memory_allocator_v2);
+    use_v1(&memory_allocator_v3);
+    use_v2(&memory_allocator_v4);
 #ifdef USE_MEMORY_DEBUG_INFO
 #if defined(VM_GLOBAL_DEBUG_INFO)
     global_statistics();
