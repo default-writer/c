8235e76b5d6dd86e10bf932e806891671f8fb13f
default-writer <funcelot@outlook.com>
2023-11-26 19:30:01 +0300

    [master] code cleanup

--- workspace/.deps/rexo/doc/js/custom.js
+++
@@ -1,6 +0,0 @@
-(function(window, document) {
-    var elements = document.querySelectorAll('a.header > code');
-    elements.forEach(function(x) {
-        x.parentElement.className += ' rx-api';
-    });
-})(window, window.document);
--- workspace/.deps/rexo/doc/src/reference/index.md
+++
@@ -1,9 +0,0 @@
-API Reference
-=============
-
-As a single-header file library, all of Rexo's public API and implementation is
-available from within a single include:
-
-```c
-#include <rexo.h>
-```
