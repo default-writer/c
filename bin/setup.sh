7d3944c62ed5646496e4ab74bbdc10cff28f87e7
default-writer <funcelot@outlook.com>
2025-02-21 06:49:05 +0300

    fix clangd user issue

--- bin/setup.sh
+++ bin/setup.sh
@@ -60,13 +60,13 @@ if [[ "$(get-os-type)" == "win" ]]; then
 fi
 
 "${pwd}/bin/utils/setup.sh" --update
-# "${pwd}/bin/utils/setup.sh" --nasm
 "${pwd}/bin/utils/setup.sh" --curl-openssl --no-update --no-upgrade
 "${pwd}/bin/utils/setup.sh" --zip --no-update --no-upgrade
 "${pwd}/bin/utils/setup.sh" --clangd --no-update --no-upgrade
 "${pwd}/bin/utils/setup.sh" --configuration --no-update --no-upgrade
-# "${pwd}/bin/utils/setup.sh" --clang-format --no-update --no-upgrade
 "${pwd}/bin/utils/setup.sh" --cmake --no-update --no-upgrade
+# "${pwd}/bin/utils/setup.sh" --clang-format --no-update --no-upgrade
+# "${pwd}/bin/utils/setup.sh" --nasm
 # "${pwd}/bin/utils/setup.sh" --gtk4 --no-update --no-upgrade
 # "${pwd}/bin/utils/setup.sh" --nodejs --no-update --no-upgrade
 # "${pwd}/bin/utils/setup.sh" --raylib --no-update --no-upgrade
